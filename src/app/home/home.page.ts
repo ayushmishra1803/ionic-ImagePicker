6ea86b5ba6e00e2789de7a52256f5710cf2d746a
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-10-21 23:47:19 +0530

    Updates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,23 +8,43 @@ import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
 })
 export class HomePage {
 	constructor(private camera: Camera) {}
-	clickedImage:string="Waiting";
-	
+	clickedImage: string = "Waiting";
+
 	click() {
-		this.camera.getPicture({
-			sourceType:this.camera.PictureSourceType.CAMERA,
-			destinationType:this.camera.DestinationType.FILE_URI
-		}).then(
-			(imageData) => {
-				// imageData is either a base64 encoded string or a file URI
-				// If it's base64 (DATA_URL):
-				this.clickedImage=imageData
-			},
-			(err) => {
-				console.log(err);
-				// Handle error
-				this.clickedImage="hua nhi bhai"
-			},
-		);
+		this.camera
+			.getPicture({
+				sourceType: this.camera.PictureSourceType.CAMERA,
+				destinationType: this.camera.DestinationType.FILE_URI,
+			})
+			.then(
+				(imageData) => {
+					// imageData is either a base64 encoded string or a file URI
+					// If it's base64 (DATA_URL):
+					this.clickedImage = imageData;
+				},
+				(err) => {
+					console.log(err);
+					// Handle error
+					this.clickedImage = "hua nhi bhai";
+				},
+			);
+	}
+	libary(){
+			this.camera
+				.getPicture({
+					sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+					destinationType: this.camera.DestinationType.FILE_URI,
+				})
+				.then(
+					(imageData) => {
+					
+						this.clickedImage = imageData;
+					},
+					(err) => {
+						console.log(err);
+						// Handle error
+						this.clickedImage = "hua nhi bhai";
+					},
+				);
 	}
 }
